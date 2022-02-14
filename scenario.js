da734e38ca130cd7782b08ec822a73211c9dbabe
Dmitry <dvasilenko1999@gmail.com>
2022-02-14 21:34:28 +0200

    v0.0

--- scenario.js
+++ scenario.js
@@ -1,6 +1,19 @@
-console.log(test);
+const randomColor = () => {
+    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return color;
+}
 
-let p = 17;
-let s = 19;
+const changeBackground = () => {
+    let body = document.querySelector("body");
+    let colorFirst = randomColor();
+    let colorSecond = randomColor();
+    body.style.background = "linear-gradient(to right, " + colorFirst + ", " + colorSecond + ")";
 
-let test = 1;
+    let text = document.querySelector("p");
+    text.innerText = `linear-gradient(to right, ${colorFirst}, ${colorSecond})`;
+
+    let button = document.querySelector("button");
+    button.addEventListener('click', changeBackground);
+}
+
+window.addEventListener('load', changeBackground);
